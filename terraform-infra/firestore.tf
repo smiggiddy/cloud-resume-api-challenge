a0bf0fee4f7f81459aa884c424487765be81b092
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2024-07-30 16:26:12 -0400

    updated

--- terraform-infra/firestore.tf
+++ terraform-infra/firestore.tf
@@ -1,8 +1,8 @@
 # Using the Default Firestore to adhere to the free quota
-resource "google_firestore_database" "database" {
-  project     = var.project_name
-  name        = "(default)"
-  location_id = "nam5"
-  type        = "FIRESTORE_NATIVE"
-}
+# resource "google_firestore_database" "database" {
+#  project     = var.project_name
+#  name        = "(default)"
+#  location_id = "nam5"
+#  type        = "FIRESTORE_NATIVE"
+# }
 
--- terraform-infra/lambda.tf
+++ terraform-infra/lambda.tf
@@ -1,14 +1,21 @@
 # File contains manifests for Bucket and cloud function
 
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = abspath("${path.module}../lambda")
+  output_path = abspath("${path.module}../lambda/function.zip")
+}
+
 resource "google_storage_bucket" "bucket" {
   name     = "smigtech-bucket"
   location = "US"
 }
 
 resource "google_storage_bucket_object" "archive" {
-  name   = "index.zip"
-  bucket = google_storage_bucket.bucket.name
-  source = abspath("${path.module}../lambda/")
+  name       = "index.zip"
+  bucket     = google_storage_bucket.bucket.name
+  source     = abspath("${path.module}../lambda/function.zip")
+  depends_on = [google_storage_bucket.bucket, data.archive_file.source]
 }
 
 resource "google_cloudfunctions_function" "function" {
@@ -21,6 +28,8 @@ resource "google_cloudfunctions_function" "function" {
   source_archive_object = google_storage_bucket_object.archive.name
   trigger_http          = true
   entry_point           = "http_handler"
+
+  depends_on = [google_storage_bucket_object.archive]
 }
 
 # IAM entry for all users to invoke the function
@@ -31,6 +40,8 @@ resource "google_cloudfunctions_function_iam_member" "invoker" {
 
   role   = "roles/cloudfunctions.invoker"
   member = "allUsers"
+
+  depends_on = [google_cloudfunctions_function.function]
 }
 
 output "http_endpoint" {
