8145b3f40201a5026d2bc1f7ab17c4ad45872193
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2024-07-30 08:43:47 -0400

    feat: add svc account

---
+++ terraform/iam.tf
@@ -0,0 +1,21 @@
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/iam.serviceAccountUser"
+
+    members = [
+      google_service_account.sa.email,
+    ]
+  }
+}
+
+resource "google_service_account" "sa" {
+  account_id   = var.service_account_id
+  display_name = var.service_account_display_name
+  email        = var.service_account_email
+}
+
+
+resource "google_service_account_iam_policy" "admin-account-iam" {
+  service_account_id = google_service_account.sa.name
+  policy_data        = data.google_iam_policy.admin.policy_data
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,22 @@
+variable "zone_name" {
+
+}
+variable "region" {
+
+}
+variable "project_name" {
+
+}
+
+variable "service_account_id" {
+
+  description = "Service Account to use with GitHub Actions"
+}
+
+variable "service_account_display_name" {
+
+}
+
+variable "service_account_email" {
+
+}
