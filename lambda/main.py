43740f223dc728507096d5cc30f55605cf7e49a3
Mike Smith <89040888+smiggiddy@users.noreply.github.com>
2024-07-30 17:15:00 -0400

    testing

--- lambda/main.py
+++ lambda/main.py
@@ -1,15 +1,19 @@
 import functions_framework
 from google.auth.environment_vars import CREDENTIALS
 from google.cloud import firestore
+from json import loads
 import os
 from uuid import uuid4
-import google.auth
+from google.oauth2 import service_account
 
-credentials, project = google.auth.default()
 
 PROJECT_NAME = os.getenv("PROJECT_NAME", "DEFAULT")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "DEFAULT")
 COLLECTION_NAME = os.getenv("COLLECTION_NAME", "DEFAULT")
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS", "NONE")
+
+json_acct_info = loads(GOOGLE_APPLICATION_CREDENTIALS)
+credentials = service_account.Credentials.from_service_account_info(json_acct_info)
 
 db = firestore.Client(
     project=PROJECT_NAME, database=DATABASE_NAME, credentials=credentials
